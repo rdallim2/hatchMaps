2b9aa283f2a39a27181a1c28c9379e5c59e12ebf
rdallim2 <ryandallimore@gmail.com>
2024-09-17 18:07:29 -0700

    Basic react app setup

---
+++ hatchmaps-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="text-center">
+        <h1>Hello fly-fishers!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
