1062879ae22200478fa6f31e4a7109a329153d56
rdallim2 <ryandallimore@gmail.com>
2024-09-19 15:37:35 -0700

    Creating user and pin objects

--- hatchmaps-app/backend/index.js
+++ hatchmaps-app/backend/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+
+
+// process.env from dotenv, to prevent link from beign displayable on git
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Mongodb Connected!");
+    }).catch((err) => console.log(err));
+
+
+
+    
 app.listen(5933, ()=>{
     console.log("Backend server is running fart.")
 })
