4a3967d4a3d1eeb55ca240c7c11a74e3788c5224
rdallim2 <ryandallimore@gmail.com>
2024-09-24 13:41:19 -0700

    Map works now upon initial load of page

--- hatchmaps-app/frontend/src/App.js
+++ hatchmaps-app/frontend/src/App.js
@@ -1,9 +1,18 @@
+import * as React from 'react';
+import Map from 'react-map-gl';
+
 function App() {
   return (
-    <div className="App">
-      Hello Hatchmaps
-    </div>
+    <Map //All this taken from documentation
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      initialViewState={{
+        latitude: 44.44667,
+        longitude: -121.69472,
+        zoom: 14
+      }}
+      style={{width: '100vw', height: '100vh'}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
   );
 }
-
 export default App;
