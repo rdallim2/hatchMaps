63239fec74475b744c1e2eba23720fa5c61b3d8a
rdallim2 <ryandallimore@gmail.com>
2024-12-15 12:14:09 -0800

    Trying to set up commection pooling to mysql

--- hatchmaps-app/server/routes/db.js
+++ hatchmaps-app/server/routes/db.js
@@ -1,5 +1,18 @@
 import mysql from "mysql2"
 
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  port: 3306
+});
+
+/*
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,  // 'db' from docker-compose
   user: process.env.DB_USER,
@@ -7,6 +20,7 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME,
   port: 3306
 });
+*/
 
 connection.connect((err) => {
     if (err) {
